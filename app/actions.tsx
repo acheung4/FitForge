222ebe6b390e373845942d881bfd51fcf4b63f05
acheung4 <acheung4@villanova.edu>
2024-04-14 16:54:35 -0400

    Implemented preliminary algorithm logic for lifting weights

--- app/actions.tsx
+++ app/actions.tsx
@@ -1,17 +1,58 @@
 'use server';
 
+import { z } from 'zod';
+
+const FormSchema = z.object( {
+    weight: z.coerce.number(),
+    feet: z.coerce.number(),
+    inches: z.coerce.number(),
+    level: z.string(),
+});
+
 export async function retrieveInput(formData: FormData) {
     
-    const rawFormData = {
+    const { weight, feet, inches, level } = FormSchema.parse({
         weight: formData.get('weight'),
         feet: formData.get('height-ft'),
         inches: formData.get('height-in'),
         level: formData.get('experience-level'),
-    };
+    });
 
-    if (rawFormData.level == 'intermediate') {
-        console.log('intermediate was chosen');
+    const heightInInches = (feet * 12) + inches;
+
+    var dataValid = true;
+
+    //checking for valid data
+    if (weight > 1000 || weight < 50) {
+        console.log('Weight falls outside expected range.');
+    }
+    if (heightInInches < 36 || heightInInches > 108) {
+        console.log('Height falls outside expected range.');
+        dataValid = false;
+    }
+    if (level == '') {
+        console.log('Experience level not selected.');
+        dataValid = false;
     }
 
-    console.log(rawFormData);
+    if (dataValid) {
+        //calculate the amount of lbs that person should lift, using weight, height, and experience lvl as multipliers
+        var lbs = 20;
+
+        lbs = lbs * (weight / 150) * (heightInInches / 75);
+
+        switch (level) {
+            case 'novice':
+                lbs = lbs * 1;
+                break;
+            case 'intermediate':
+                lbs = lbs * 1.25;
+                break;
+            case 'expert':
+                lbs = lbs * 1.5;
+        }
+
+        console.log(lbs.toFixed(2));
+    }
+
 }
