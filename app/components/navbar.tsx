d358de7e8385778b071bf0d0385d64b8f9278121
MattT52 <mtopolew@villanova.edu>
2024-04-19 17:52:24 -0400

    First draft of community page (will revise/research

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function NavBar() {
     return (
         <div className="flex justify-between pb-[10px] ">
-            <Link href={'/'}>Home</Link>
-            <Link href={'/community'}>Community Board</Link>
-            <Link href={'/workouts'}>List of Workouts</Link>
+            <Link href={'/'}>Home </Link>
+            <Link href={'/community'}>Community Board </Link>
+            <Link href={'/workouts'}>List of Workouts </Link>
             <Link href={'/create-workout'}>
                Create new workout plan
             </Link>
