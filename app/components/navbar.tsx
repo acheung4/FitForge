b9068f82399f8ce132c9756dda57a1af9063151e
Dmitri Gaspard <113148317+Dmitri50@users.noreply.github.com>
2024-04-19 17:00:33 -0400

    fixed the get started button on the home page

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,13 +2,15 @@ import Link from "next/link";
 
 export default function NavBar() {
     return (
-        <div className="justify-between">
-            <Link href={'/'}>Home</Link>
-            <Link href={'/community'}>Community Board</Link>
-            <Link href={'/workouts'}>List of Workouts</Link>
-            <Link href={'/create-workout'}>
-               Create new workout plan
-            </Link>
+        <div className="flex justify-end w-full mt-px">
+            <div className="flex space-x-4">
+                <Link href={'/'}>Home</Link>
+                <Link href={'/community'}>Community Board</Link>
+                <Link href={'/workouts'}>List of Workouts</Link>
+                <Link href={'/create-workout'}>
+                Create new workout plan
+                </Link>
+            </div>
         </div>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,13 @@ import NavBar from '@/app/components/navbar';
 export default function Home() {
   return (
     <div className="">
-      <button className="text-lg">
-        <div className='bg-blue-500 border rounded-full mx-[590px] text-center my-[350px] h-[220px] w-[390px]'>
+      <img src="/public/FitForge-logo.svg" alt="Logo" className='top-0' style={{ height: '360px', width: '390px', margin: '150px auto' }}/>
+      <button className="font-bold flex justify-center items-center" style={{ height: '220px', width: '390px', margin: '190px auto' }}>
+        <div className="bg-blue-500 border rounded-full text-center text-6x1 h-full w-full flex items-center justify-center">
           Get Started
         </div>
       </button>
+
       {/* <NavBar /> */}
     </div>
   );
