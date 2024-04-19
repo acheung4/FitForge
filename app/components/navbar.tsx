a4efa2cb100a17934248157c426b0322064f054a
Dmitri Gaspard <113148317+Dmitri50@users.noreply.github.com>
2024-04-18 21:54:24 -0400

    Updating the pages

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NavBar() {
     return (
-        <div className="flex justify-between pb-[10px] ">
+        <div className="justify-between">
             <Link href={'/'}>Home</Link>
             <Link href={'/community'}>Community Board</Link>
             <Link href={'/workouts'}>List of Workouts</Link>
--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import NavBar from '@/app/components/navbar';
 
 export default function Home() {
   return (
-    <div className="self-stretch px-[130px] flex justify-between items-center pb-[50px]">
+    <div className="flex justify-between items-center">
       <h1></h1>
-      <button className="curson-pointer rounded-lg px-[10px] py-[8px] h-[40px] w-[93px]">
-        <div className='bg-blue-500 border relative font-medium font-poppins text-black my-[300px]'>
+      <button className="curson-pointer items-center rounded px-[0px] py-[12px] h-[50px] w-[120px] text-lg">
+        <div className='bg-blue-500 border rounded-full mx-[190px] relative font-medium font-poppins items-center justify-center text-center text-black my-[350px] h-[220px] w-[390px] text-lg'>
           Get Started
         </div>
       </button>
