76aad31f9a87b77caac9e8c05915a3203a0aa745
Dmitri Gaspard <113148317+Dmitri50@users.noreply.github.com>
2024-04-18 22:04:34 -0400

    updating pages

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import NavBar from '@/app/components/navbar';
 export default function Home() {
   return (
     <div className="flex justify-between items-center">
-      <h1></h1>
       <button className="curson-pointer items-center rounded px-[0px] py-[12px] h-[50px] w-[120px] text-lg">
         <div className='bg-blue-500 border rounded-full mx-[190px] relative font-medium font-poppins items-center justify-center text-center text-black my-[350px] h-[220px] w-[390px] text-lg'>
           Get Started
