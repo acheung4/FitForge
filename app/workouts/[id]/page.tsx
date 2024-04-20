38e32102c094f0b71b0af3ebaa6ea7d9266f0a73
acheung4 <acheung4@villanova.edu>
2024-04-20 01:31:14 -0400

    Updated design for specific workout page

--- app/workouts/[id]/page.tsx
+++ app/workouts/[id]/page.tsx
@@ -10,15 +10,65 @@ export default async function SpecificWorkout(
 
     return (
         workout ? (
-            <WorkoutDetails
-                key={workout.id}
-                id={workout.id}
-                title={workout.title}
-                monday={workout.monday}
-                tuesday={workout.tuesday}
-                wednesday={workout.wednesday}
-                thursday={workout.thursday}
-                friday={workout.friday}
-            />) : null
+            <section className="h-screen">
+                <div className="max-w-screen-xl mx-auto px-6 py-24">
+                    
+                    <div className="mx-auto text-center">
+                        <h2 className="text-4xl font-extrabold text-gray-900">
+                            {workout.title}
+                        </h2>
+                        <h2 className="text-2xl font-bold text-gray-700">
+                            {workout.createdAt.toLocaleString()}
+                        </h2>
+                    </div>
+
+                    <div className="flow-root max-w-3xl mx-auto mt-16">
+                        <div className="-my-4 divide-y divide-gray-700">
+                            
+                            <div className="flex py-4 gap-6 items-center">
+                                <p className="w-32 text-lg font-normal sm:text-right text-gray-400 shrink-0">
+                                    MONDAY
+                                </p>
+                                <h3 className="text-lg font-semibold text-gray-700">
+                                    {workout.monday}
+                                </h3>
+                            </div>
+                            <div className="flex py-4 gap-6 items-center">
+                                <p className="w-32 text-lg font-normal sm:text-right text-gray-400 shrink-0">
+                                    TUESDAY
+                                </p>
+                                <h3 className="text-lg font-semibold text-gray-700">
+                                    {workout.tuesday}
+                                </h3>
+                            </div>
+                            <div className="flex py-4 gap-6 items-center">
+                                <p className="w-32 text-lg font-normal sm:text-right text-gray-400 shrink-0">
+                                    WEDNESDAY
+                                </p>
+                                <h3 className="text-lg font-semibold text-gray-700">
+                                    {workout.wednesday}
+                                </h3>
+                            </div>
+                            <div className="flex py-4 gap-6 items-center">
+                                <p className="w-32 text-lg font-normal sm:text-right text-gray-400 shrink-0">
+                                    THURSDAY
+                                </p>
+                                <h3 className="text-lg font-semibold text-gray-700">
+                                    {workout.thursday}
+                                </h3>
+                            </div>
+                            <div className="flex py-4 gap-6 items-center">
+                                <p className="w-32 text-lg font-normal sm:text-right text-gray-400 shrink-0">
+                                    FRIDAY
+                                </p>
+                                <h3 className="text-lg font-semibold text-gray-700">
+                                    {workout.friday}
+                                </h3>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </section>) : null
     );
 }
