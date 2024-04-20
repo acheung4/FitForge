0fefa6ed0dd8e263c2448ca6d1db475c93aa7ce5
acheung4 <acheung4@villanova.edu>
2024-04-20 19:31:35 -0400

    Modified algorithm cutoff values

--- app/actions.tsx
+++ app/actions.tsx
@@ -6,7 +6,7 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
 export async function createWorkout(prevState: any, formData: FormData) {
-    
+
     let workoutID;
 
     const schema = z.object({
@@ -30,7 +30,7 @@ export async function createWorkout(prevState: any, formData: FormData) {
 
         const heightInInches = (data.feet * 12) + data.inches;
 
-        let index = 20 * (data.weight / 150) * (heightInInches / 75);
+        let index = 10 * (data.weight / 150) * (heightInInches / 75);
 
         switch (data.level) {
             case 'novice':
@@ -55,7 +55,7 @@ export async function createWorkout(prevState: any, formData: FormData) {
                 friday: friday,
             },
         });
-        
+
         if (data.community != 0) {
             const linkCommunity = await db.community.update({
                 where: {
@@ -72,7 +72,7 @@ export async function createWorkout(prevState: any, formData: FormData) {
         }
         revalidatePath('/', 'layout');
         workoutID = workout.id;
-        
+
     }
     catch (e) {
         if (e instanceof ZodError) {
@@ -85,30 +85,30 @@ export async function createWorkout(prevState: any, formData: FormData) {
         else {
             return { message: 'Failed to add workout' };
         }
-        
+
     }
-    
+
     redirect(`/workouts/${workoutID}`);
 }
 
 function generateWorkout(index: number) {
     let monday, tuesday, wednesday, thursday, friday;
 
-    if (index < 5) {
+    if (index < 8) {
         monday = 'Bodyweight Squats, 3 sets of 12 reps';
         tuesday = 'Push-ups, 3 sets of 15 reps';
         wednesday = 'Bodyweight Pull-ups, 3 sets of 10 reps';
         thursday = 'Plank, 3 sets of 30 seconds';
         friday = 'Jogging, 20 minutes';
     }
-    else if (index < 10) {
+    else if (index < 12) {
         monday = 'Deadlift, 4 sets of 10 reps';
         tuesday = 'Bench Press, 4 sets of 8 reps';
         wednesday = 'Bent Over Row, 4 sets of 10 reps';
         thursday = 'Leg Press, 4 sets of 12 reps';
         friday = 'HIIT Cardio, 15 minutes';
     }
-    else if (index < 15) {
+    else if (index < 16) {
         monday = 'Clean and Jerk, 5 sets of 5 reps';
         tuesday = 'Snatch, 5 sets of 5 reps';
         wednesday = 'Front Squat, 5 sets of 5 reps';
@@ -125,3 +125,41 @@ function generateWorkout(index: number) {
 
     return { monday, tuesday, wednesday, thursday, friday };
 }
+
+// export async function testIndex(prevState: any, formData: FormData) {
+
+//     const schema = z.object({
+//         title: z.string().min(1),
+//         weight: z.coerce.number().gte(50).lte(1000),
+//         feet: z.coerce.number().gte(3).lte(8),
+//         inches: z.coerce.number().gte(0).lt(12),
+//         level: z.string(),
+//         community: z.coerce.number(),
+//     });
+
+//     const data = schema.parse({
+//         title: formData.get('title'),
+//         weight: formData.get('weight'),
+//         feet: formData.get('height-ft'),
+//         inches: formData.get('height-in'),
+//         level: formData.get('experience-level'),
+//         community: formData.get('community'),
+//     });
+
+//     const heightInInches = (data.feet * 12) + data.inches;
+
+//     let index = 10 * (data.weight / 150) * (heightInInches / 75);
+
+//     switch (data.level) {
+//         case 'novice':
+//             index = index * 1;
+//             break;
+//         case 'intermediate':
+//             index = index * 1.25;
+//             break;
+//         case 'expert':
+//             index = index * 1.5;
+//     }
+
+//     return { message: index.toString() };
+// }
