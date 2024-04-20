bec4c280e7bb82d2259b3782442a3777cb0d2a9a
Dmitri Gaspard <113148317+Dmitri50@users.noreply.github.com>
2024-04-20 18:28:20 -0400

    Changed the layout of the selection screen

--- app/create-workout/form.tsx
+++ app/create-workout/form.tsx
@@ -2,6 +2,8 @@
 
 import { createWorkout } from '@/app/actions';
 import { useFormState, useFormStatus } from 'react-dom';
+import FitForgeLogo from "@/public/FitForge-logo.svg"
+import Image from 'next/image';
 
 const initialState = {
     message: "",
@@ -21,41 +23,57 @@ export default function Form({
     }
 
     return (
-        <form action={formAction} className='mt-60'>
-            <div className='mb-4'>
-                <label htmlFor="title">Enter workout title: </label>
-                <input type="text" id="title" name="title" required></input>
+        <form action={formAction} className='w-full max-w-lg mt-60'>
+            <div className='flex flex-wrap -mx-3 mb-6'>
+                <div className='w-full px-3'>
+                    <label htmlFor="title" className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>
+                        Enter workout title: </label>
+                    <input type="text" id="title" name="title" required className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'>                       
+                    </input>
+                </div>
             </div>
 
-            <div className='mb-4'>
-                <label htmlFor="weight">Enter weight in pounds: </label>
-                <input type="number" id="weight" name="weight" required></input>
+            <div className='flex flex-wrap -mx-3 mb-6'>
+                <div className='w-full px-3'>
+                    <label htmlFor="weight" className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>
+                        Enter weight in pounds: </label>
+                    <input type="number" id="weight" name="weight" required className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'>
+                    </input>
+                </div>
             </div>
 
-            <div className='mb-4'>
-                <label htmlFor="height">Enter height: </label><br></br>
-                <span>Feet: </span>
-                <input type="number" id="height-ft" name="height-ft" required></input>
-                <span>Inches: </span>
-                <input type="number" id="height-in" name="height-in" required></input>
+            <div className='flex flex-wrap -mx-3 mb-6'>
+                <div className='w-full md:w-1/2 px-3 mb-6 md:mb:-0'>
+                    <label htmlFor="height" className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>Enter height: </label><br></br>
+                    <span>Feet: </span>
+                    <input type="number" id="height-ft" name="height-ft" required className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'>                        
+                    </input>
+                    <div className='w-full md:w-1/2 px-3'>
+                        <span>Inches: </span>
+                        <input type="number" id="height-in" name="height-in" required className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'>                            
+                        </input>
+                    </div>
+                </div>
             </div>
 
-            <div className='mb-4'>
-                <label htmlFor="experience-level">Enter experience level: </label>
-                <select id='experience-level' name='experience-level' defaultValue='' required>
-                    <option value='' disabled>
-                        Select your experience level
-                    </option>
-                    <option key='novice' value='novice' className='text-black'>
-                        Novice
-                    </option>
-                    <option key='intermediate' value='intermediate' className='text-black'>
-                        Intermediate
-                    </option>
-                    <option key='expert' value='expert' className='text-black'>
-                        Expert
-                    </option>
-                </select>
+            <div className='flex flex-wrap -mx-3 mb-6'>
+                <div className='w-full px-3'>
+                    <label htmlFor="experience-level" className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>Enter experience level: </label>
+                    <select id='experience-level' name='experience-level' defaultValue='' required className='block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500'>
+                        <option value='' disabled>
+                            Select your experience level
+                        </option>
+                        <option key='novice' value='novice' className='text-black'>
+                            Novice
+                        </option>
+                        <option key='intermediate' value='intermediate' className='text-black'>
+                            Intermediate
+                        </option>
+                        <option key='expert' value='expert' className='text-black'>
+                            Expert
+                        </option>
+                    </select>
+                </div>
             </div>
 
             {/* community drop down */}
