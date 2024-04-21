a378751043e77dac4f573e300994a10d5fa04908
acheung4 <acheung4@villanova.edu>
2024-04-20 21:16:45 -0400

    Added basic design for community board

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -1,56 +1,26 @@
 import { getCommunities } from "@/database/queries";
+import Link from "next/link";
 
 export default async function CommunityBoard() {
-    
+
     const communities = await getCommunities();
-    
+
     return (
-        communities.map((community) => {
-            return (
-                <div>
-                    <h2>{community.name}</h2>
-                </div>
-            )
-        })
+        <div>
+            <p className="text-4xl font-bold mb-8 mt-8 text-center">
+                Community Board
+            </p>
+            <div className="grid grid-cols-4 gap-4 m-4">
+                {communities.map((community) => {
+                    return (
+                        <Link key={community.id} href={`/community/${community.id}`} className="block max-w-sm p-6 bg-white border rounded-lg shadow border-gray-700 hover:bg-blue-600">
+                            <p>
+                                {community.name}
+                            </p>
+                        </Link>
+                    );
+                })}
+            </div>
+        </div>
     );
 }
-
-// export default function CommunityBoard() {
-//     return (
-//         <div style={{ display: 'flex', height: '100vh' }}>
-//     {/* Vertical navbar */}
-//         <div style={{ width: '200px', backgroundColor: '#36393f', color: '#fff', padding: '20px' }}>
-//         <h2 style={{ marginBottom: '20px' }}>Gyms</h2>
-//         <ul style={{ listStyleType: 'none', padding: 0 }}>
-//           <li>Planet Fitness</li>
-//           <li>LA Fitness</li>
-//           <li>Barbell Brigade</li>
-//     {/* Add more gyms here */}
-//         </ul>
-//       </div>
-//     {/* Main content area */}
-//       <div style={{ flex: 1, padding: '20px', overflowY: 'scroll' }}>
-//         <h1>Workout Community</h1>
-//         {/* Posts */}
-//         <div>
-//           {posts.map(post => (
-//             <div key={post.id}>
-//               <h2>{post.title}</h2>
-//               <p>{post.body}</p>
-//             </div>
-//           ))}
-//         </div>
-//         {/* New post form */}
-//         <div>
-//           <textarea
-//             value={newPostText}
-//             onChange={e => setNewPostText(e.target.value)}
-//             placeholder="Type your message..."
-//             style={{ width: '100%', height: '100px', marginBottom: '10px' }}
-//           />
-//           <button onClick={handlePostSubmit}>Submit</button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
